3de24756e710437602cbc5169c21c452071c63ec
stepandenisov <stdnsv@gmail.com>
2025-05-15 00:50:50 +0400

    Add image controller tests

---
+++ src/test/java/ru/yandex/blog/controller/ImageControllerIntegrationTest.java
@@ -0,0 +1,60 @@
+package ru.yandex.blog.controller;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import ru.yandex.blog.configuration.DataSourceConfiguration;
+import ru.yandex.blog.configuration.WebConfiguration;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringJUnitConfig(classes = {DataSourceConfiguration.class, WebConfiguration.class})
+@WebAppConfiguration
+@TestPropertySource(locations = "classpath:test-application.properties")
+public class ImageControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+        // Очистка и заполнение тестовых данных в базе
+        jdbcTemplate.execute("TRUNCATE TABLE images RESTART IDENTITY");
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY=0");
+        jdbcTemplate.execute("TRUNCATE TABLE posts RESTART IDENTITY");
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY=1");
+
+        jdbcTemplate.execute("insert into posts(title, post_text, likes_count) values ('First post', 'Text of the first post', 0);");
+        jdbcTemplate.execute("insert into posts(title, post_text, likes_count) values ('Second post', 'Text of the second post', 0);");
+        jdbcTemplate.execute("insert into posts(title, post_text, likes_count) values ('Third post', 'Text of the third post', 0);");
+
+        jdbcTemplate.execute("insert into images(post_id, image) values (1, file_read('D:\\Projects\\myblog\\src\\main\\resources\\assets\\1.jpg'));");
+        jdbcTemplate.execute("insert into images(post_id, image) values (2, file_read('D:\\Projects\\myblog\\src\\main\\resources\\assets\\2.jpg'));");
+        jdbcTemplate.execute("insert into images(post_id, image) values (3, file_read('D:\\Projects\\myblog\\src\\main\\resources\\assets\\3.jpg'));");
+    }
+
+    @Test
+    void add_shouldAddImageToPostAndRedirect() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/images/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_OCTET_STREAM));
+    }
+
+}
