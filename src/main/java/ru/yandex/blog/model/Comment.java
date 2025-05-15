ca5cc1281dc70bfff3f8f38d3aa5ac3deb1990a3
stepandenisov <stdnsv@gmail.com>
2025-05-15 17:00:23 +0400

    Add "equals" method for Paging, Comment, PostDto classes

--- src/main/java/ru/yandex/blog/model/Comment.java
+++ src/main/java/ru/yandex/blog/model/Comment.java
@@ -1,11 +1,13 @@
 package ru.yandex.blog.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class Comment {
     private int id;
     private int postId;
--- src/main/java/ru/yandex/blog/model/Paging.java
+++ src/main/java/ru/yandex/blog/model/Paging.java
@@ -1,11 +1,13 @@
 package ru.yandex.blog.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class Paging {
     @Setter
     private int pageNumber;
